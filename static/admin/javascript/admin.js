31da9f422b44defe3f8e9dc049156511ce16ed5c
Evan-Ferro <efarro@northeastmaritime.com>
2024-07-16 07:29:43 -0400

    Began formatting for user management

--- static/admin/javascript/admin.js
+++ static/admin/javascript/admin.js
@@ -1,5 +1,5 @@
-/* Sidebar Toggle */
 document.addEventListener("DOMContentLoaded", function () {
+    /* Sidebar Toggle */
     const headers = document.querySelectorAll(".collapsible-header");
 
     headers.forEach(header => {
@@ -41,5 +41,90 @@ document.addEventListener("DOMContentLoaded", function () {
             // Add a class to the link element if the paths match
             linkElement.classList.add("current-page");
         }
+    });
+
+    /* Select Table Options */
+    let checkedOptionsList = [];
+    const tableOptions = document.querySelectorAll('.table-select-option');
+    const tableSelectAllOption = document.querySelector('.table-select-all-option');
+
+    tableOptions.forEach(option => {
+        option.addEventListener('click', function() {
+            const checkbox = this.querySelector('input[type="checkbox"]');
+            if (checkbox && checkbox.checked) {
+                checkbox.checked = false;
+                option.classList.remove('selected-option');
+                checkedOptionsList = checkedOptionsList.filter(item => item !== option);
+            } else {
+                checkbox.checked = true;
+                option.classList.add('selected-option');
+                checkedOptionsList.push(option);
+            }
+            if(tableOptions.length === checkedOptionsList.length){
+                tableSelectAllOption.checked = true;
+            }else{
+                tableSelectAllOption.checked = false;
+            }
+            countSelectedOptions();
+        });
+    });
+
+    tableSelectAllOption.addEventListener('click', function() {
+        checkedOptionsList = [];
+        tableOptions.forEach(option => {
+            const checkbox = option.querySelector('input[type="checkbox"]');
+            if(tableSelectAllOption.checked){
+                checkbox.checked = true;
+                option.classList.add('selected-option');
+                checkedOptionsList.push(option);
+            }else {
+                checkbox.checked = false;
+                option.classList.remove('selected-option');
+                checkedOptionsList = checkedOptionsList.filter(item => item !== option);
+            }
+            countSelectedOptions();
+        });
+    });
+
+    const selectedOptionsWrapper = document.getElementById('selectedOptionsWrapper');
+    const selectedOptionsCount = document.getElementById('selectedOptionsCount');
+    function countSelectedOptions(){
+        if(checkedOptionsList.length < 1){
+            selectedOptionsWrapper.style.display = 'none';
+            closeOptionsSidebar();
+        }else{
+            selectedOptionsWrapper.style.display = 'flex';
+            openOptionsSidebar();
+        };
+        selectedOptionsCount.innerText = checkedOptionsList.length;
+        testActiveOptions();
+    }
+
+    /* Test active filters on page load */
+    // testActiveOptions();
+    /* Need to add functionality to test for active filters */
+    const activeFilters = document.getElementById('activeFilters');
+    function testActiveOptions(){
+        if(checkedOptionsList.length >= 1 /*|| filterActive*/){
+            activeFilters.classList.add('show-table-menu');
+        }else{
+            activeFilters.classList.remove('show-table-menu');
+        }
+    }
+
+    const optionsSidebar = document.getElementById('optionsSidebar');
+    function openOptionsSidebar(){
+        optionsSidebar.classList.add('show-options-sidebar');
+    }
+
+    function closeOptionsSidebar(){
+        optionsSidebar.classList.remove('show-options-sidebar');
+    }
+
+    // JavaScript to set custom tooltip text
+    document.querySelectorAll('.tooltip').forEach(function(elem) {
+        const tooltipText = elem.getAttribute('data-tooltip');
+        const tooltipSpan = elem.querySelector('.tooltiptext');
+        tooltipSpan.textContent = tooltipText;
     });
 });
