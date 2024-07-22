e0e8418769aa15dd54a7a3014083ca98b0153b60
Evan-Ferro <efarro@northeastmaritime.com>
2024-07-22 09:04:59 -0400

    Added dynamic filter functions

--- halo_lms/settings.py
+++ halo_lms/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'halo_lms',
 ]
 
 MIDDLEWARE = [
--- static/admin/javascript/admin.js
+++ static/admin/javascript/admin.js
@@ -100,12 +100,13 @@ document.addEventListener("DOMContentLoaded", function () {
         testActiveOptions();
     }
 
-    /* Test active filters on page load */
-    // testActiveOptions();
-    /* Need to add functionality to test for active filters */
     const activeFilters = document.getElementById('activeFilters');
+    var activeFiltersContainer = document.querySelector('.table-active-filters');
+    var filters = activeFiltersContainer.querySelectorAll('.filter');
+    /* Test active filters on page load */
+    testActiveOptions();
     function testActiveOptions(){
-        if(checkedOptionsList.length >= 1 /*|| filterActive*/){
+        if(checkedOptionsList.length >= 1 || filters.length > 0){
             activeFilters.classList.add('show-table-menu');
         }else{
             activeFilters.classList.remove('show-table-menu');
@@ -127,4 +128,146 @@ document.addEventListener("DOMContentLoaded", function () {
         const tooltipSpan = elem.querySelector('.tooltiptext');
         tooltipSpan.textContent = tooltipText;
     });
+
+    // Custom Select Box
+    var customSelects = document.querySelectorAll('.custom-select');
+
+    customSelects.forEach(function(customSelect) {
+        var selectSelected = customSelect.querySelector('.select-selected');
+        var selectItems = customSelect.querySelector('.select-items');
+
+        // Toggle the dropdown
+        selectSelected.addEventListener('click', function() {
+            // Close any open dropdowns
+            customSelects.forEach(function(otherSelect) {
+                if (otherSelect !== customSelect) {
+                    otherSelect.querySelector('.select-items').style.display = 'none';
+                    otherSelect.querySelector('.select-selected').classList.remove('select-open');
+                }
+            });
+            selectSelected.classList.toggle('select-open');
+            selectItems.style.display = selectItems.style.display === 'block' ? 'none' : 'block';
+        });
+
+        // Close the dropdown if the user clicks outside of it
+        document.addEventListener('click', function(event) {
+            if (!customSelect.contains(event.target)) {
+                selectItems.style.display = 'none';
+                selectSelected.classList.remove('select-open');
+            }
+        });
+
+        // Handle item selection
+        selectItems.querySelectorAll('div').forEach(function(item) {
+            item.addEventListener('click', function() {
+                // Get the field name and value from data attributes
+                var fieldName = this.getAttribute('data-name');
+                var fieldValue = this.getAttribute('data-value');
+
+                // Update the select box text and field value input
+                selectSelected.textContent = this.textContent;
+                selectItems.querySelectorAll('div').forEach(function(el) {
+                    el.classList.remove('same-as-selected');
+                });
+                this.classList.add('same-as-selected');
+                selectItems.style.display = 'none';
+
+                // Update the field name and value in the input
+                var fieldValueInput = document.getElementById('fieldValue');
+                if (fieldName) {
+                    fieldValueInput.name = fieldName;
+                    fieldValueInput.value = fieldValue;
+                }
+            });
+        });
+    });
+    // Showing / Hiding search option for all users 
+    var searchTable = document.getElementById('searchTable');
+    var searchAllUsersBtn = document.getElementById('searchAllUsersBtn');
+
+    searchTable.addEventListener('input', function () {
+        if(searchTable.value.length > 0){
+            searchAllUsersBtn.style.display = "flex";
+        }else{
+            searchAllUsersBtn.style.display = "none";
+        }
+    });
+
+    // Removing filters
+    const removeButtons = document.querySelectorAll('.table-active-filters .remove-filter');
+
+    removeButtons.forEach(function(button) {
+        button.addEventListener('click', function(event) {
+            // Get the parent filter div
+            const filterDiv = button.closest('.filter');
+            const key = filterDiv.getAttribute('data-key');
+            const value = filterDiv.getAttribute('data-value');
+
+            // Log the filter to be removed (for debugging)
+            
+
+            // Find the matching filter option in the custom-select div
+            const filterOptions = document.querySelectorAll('.custom-select .select-items div');
+            filterOptions.forEach(function(option) {
+                if (option.getAttribute('data-name') === 'filter_' + key) {
+                    // Update UI or perform any other necessary actions
+                    document.getElementById('fieldValue').name = option.getAttribute('data-name');
+                    document.getElementById('fieldValue').value = '';
+
+                    // filterDiv.remove();
+                    setTimeout(() => {
+                        document.getElementById('filtersForm').submit();
+                    }, 100);
+                }else if(key === 'query'){
+                    document.getElementById('searchTable').value = '';
+                    document.getElementById('searchAllUsersBtn').click();
+                }
+            });
+        });
+    });
+    // Editing filters
+    const editButtons = document.querySelectorAll('.table-active-filters .edit-filter');
+
+    editButtons.forEach(function(button) {
+        button.addEventListener('click', function(event) {
+            // Get the parent filter div
+            const filterDiv = button.closest('.filter');
+            const key = filterDiv.getAttribute('data-key');
+            const value = filterDiv.getAttribute('data-value');
+
+            // Log the filter to be removed (for debugging)
+            
+
+            // Find the matching filter option in the custom-select div
+            const filterOptions = document.querySelectorAll('.custom-select .select-items div');
+            filterOptions.forEach(function(option) {
+                if (option.getAttribute('data-name') === 'filter_' + key) {
+                    option.click();                   
+
+                    openPopup('filtersPopup');
+                }else if(key === 'query'){
+                    document.getElementById('searchTable').focus();
+                }
+            });
+        });
+    });
 });
+
+// Dynamically Opening Popups
+function openPopup(popup){
+    const currentPopup = document.getElementById(popup);
+    const popupContent = currentPopup.querySelector('.popup-content');
+    currentPopup.style.display = "flex";
+    setTimeout(() => {
+        popupContent.classList.add('animate-popup-content');
+    }, 100);
+}
+
+function closePopup(popup){
+    const currentPopup = document.getElementById(popup);
+    const popupContent = currentPopup.querySelector('.popup-content');
+    popupContent.classList.remove('animate-popup-content');
+    setTimeout(() => {
+        currentPopup.style.display = "none";
+    }, 200);
+}
