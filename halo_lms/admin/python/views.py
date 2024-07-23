40110e3d840e511bc0ba7e18f98c9d23f873a77e
Evan-Ferro <efarro@northeastmaritime.com>
2024-07-23 11:28:47 -0400

    Adjusted user views and table

--- halo_lms/admin/python/views.py
+++ halo_lms/admin/python/views.py
@@ -83,7 +83,6 @@ def admin_users(request):
     return render(request, 'admin/html/users/users.html', {
         'page_obj': page_obj,
         'active_filters': active_filters,
-<<<<<<< HEAD
         'sort_by': sort_by,
     })
 
@@ -110,57 +109,3 @@ def user_details(request, user_id):
         'user': user
     }
     return render(request, 'admin/html/users/user_details.html', context)
-=======
-=======
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
-from django.core.paginator import Paginator
-from django.db.models import Q
-from django.shortcuts import render
-
-@login_required
-def admin_dashboard(request):
-    return render(request, 'admin/html/dashboard.html')
-
-# Users
-@login_required
-def admin_users(request):
-    query_string = request.GET.get('query')
-
-    query = Q()
-    active_filters = {}
-
-    # Apply the general search query if provided
-    if query_string:
-        query &= (
-            Q(username__icontains=query_string) |
-            Q(email__icontains=query_string) |
-            Q(first_name__icontains=query_string) |
-            Q(last_name__icontains=query_string)
-        )
-        active_filters['query'] = query_string  # Track the general search query
-
-    # Build the query dynamically based on the provided filter parameters
-    for key, value in request.GET.items():
-        if key.startswith('filter_') and value:
-            field_name = key[len('filter_'):]  # Extract field name after 'filter_'
-            query &= Q(**{f"{field_name}__icontains": value})
-
-            
-            # formatted_field_name = field_name.replace('_', ' ').capitalize()
-            # active_filters[formatted_field_name] = value  
-            active_filters[field_name] = value  
-
-    users_list = User.objects.filter(query)
-
-    # Paginate the filtered users_list
-    paginator = Paginator(users_list, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-
-    # Render the results with the active filters
-    return render(request, 'admin/html/users/users.html', {
-        'page_obj': page_obj,
-        'active_filters': active_filters,
-    })
->>>>>>> b83779d0773e6480e140931db1196406a7317ef1
