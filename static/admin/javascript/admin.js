4f2abaaf4972204d6f986c19700532e6c3f76c65
Evan-Ferro <efarro@northeastmaritime.com>
2024-07-23 10:14:42 -0400

    created user details page and added sidebar functions

--- static/admin/javascript/admin.js
+++ static/admin/javascript/admin.js
@@ -96,8 +96,26 @@ document.addEventListener("DOMContentLoaded", function () {
             selectedOptionsWrapper.style.display = 'flex';
             openOptionsSidebar();
         };
+        if(checkedOptionsList.length < 2){
+            singleOptionsSelect();
+        }else{
+            multiOptionsSelect();
+        };
         selectedOptionsCount.innerText = checkedOptionsList.length;
         testActiveOptions();
+    }
+    function singleOptionsSelect(){
+        // Setting Link on Edit User Option
+        document.getElementById('singleEditUser').addEventListener('click', () => {
+            window.location.href = checkedOptionsList[0].getAttribute('data-details');
+        });
+        document.getElementById('singleOptionsSelect').style.display = 'flex';
+        document.getElementById('multiOptionsSelect').style.display = 'none';
+    }
+
+    function multiOptionsSelect(){
+        document.getElementById('singleOptionsSelect').style.display = 'none';
+        document.getElementById('multiOptionsSelect').style.display = 'flex';
     }
 
     const activeFilters = document.getElementById('activeFilters');
@@ -131,6 +149,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Custom Select Box
     var customSelects = document.querySelectorAll('.custom-select');
+    var sortButton = document.getElementById('sortButton');
+    var sortForm = document.getElementById('sortForm');
+    var sortByInput = document.getElementById('sort_by');
 
     customSelects.forEach(function(customSelect) {
         var selectSelected = customSelect.querySelector('.select-selected');
@@ -151,7 +172,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Close the dropdown if the user clicks outside of it
         document.addEventListener('click', function(event) {
-            if (!customSelect.contains(event.target)) {
+            if (!customSelect.contains(event.target) && selectItems.id != 'sortSelectItems') {
                 selectItems.style.display = 'none';
                 selectSelected.classList.remove('select-open');
             }
@@ -159,28 +180,69 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Handle item selection
         selectItems.querySelectorAll('div').forEach(function(item) {
-            item.addEventListener('click', function() {
-                // Get the field name and value from data attributes
-                var fieldName = this.getAttribute('data-name');
-                var fieldValue = this.getAttribute('data-value');
-
-                // Update the select box text and field value input
-                selectSelected.textContent = this.textContent;
-                selectItems.querySelectorAll('div').forEach(function(el) {
-                    el.classList.remove('same-as-selected');
+            if (selectItems.id === 'sortSelectItems') {
+                item.addEventListener('click', function() {
+                    var selectedValue = this.getAttribute('data-value');
+                    sortByInput.value = selectedValue;
+                    sortForm.submit();
                 });
-                this.classList.add('same-as-selected');
-                selectItems.style.display = 'none';
+            } else {
+                item.addEventListener('click', function() {
+                    // Get the field name and value from data attributes
+                    var fieldName = this.getAttribute('data-name');
+                    var fieldValue = this.getAttribute('data-value');
 
-                // Update the field name and value in the input
-                var fieldValueInput = document.getElementById('fieldValue');
-                if (fieldName) {
-                    fieldValueInput.name = fieldName;
-                    fieldValueInput.value = fieldValue;
-                }
-            });
+                    // Update the select box text and field value input
+                    selectSelected.textContent = this.textContent;
+                    selectItems.querySelectorAll('div').forEach(function(el) {
+                        el.classList.remove('same-as-selected');
+                    });
+                    this.classList.add('same-as-selected');
+                    selectItems.style.display = 'none';
+
+                    // Update the field name and value in the input
+                    var fieldValueInput = document.getElementById('fieldValue');
+                    if (fieldName) {
+                        fieldValueInput.name = fieldName;
+                        fieldValueInput.value = fieldValue;
+                    }
+                });
+            }
         });
     });
+
+    // Open the sort dropdown when the sort button is clicked
+    sortButton.addEventListener('click', function() {
+        // Close any open custom selects
+        customSelects.forEach(function(customSelect) {
+            customSelect.querySelector('.select-items').style.display = 'none';
+            customSelect.querySelector('.select-selected').classList.remove('select-open');
+        });
+
+        // Toggle the sort dropdown
+        sortSelectItems.style.display = sortSelectItems.style.display === 'block' ? 'none' : 'block';
+    });
+
+    // Close the sort dropdown if the user clicks outside of it
+    document.addEventListener('click', function(event) {
+        var sortByParent = event.target.closest('[data-key="sort_by"]');
+        if (!sortButton.contains(event.target) && !sortSelectItems.contains(event.target) && !sortByParent) {
+            sortSelectItems.style.display = 'none';
+        }
+    });
+
+    // Close the dropdown if the user clicks outside of it
+    document.addEventListener('click', function(event) {
+        customSelects.forEach(function(customSelect) {
+            var selectItems = customSelect.querySelector('.select-items');
+            var selectSelected = customSelect.querySelector('.select-selected');
+            if (!customSelect.contains(event.target) && selectItems.id !== 'sortSelectItems') {
+                selectItems.style.display = 'none';
+                selectSelected.classList.remove('select-open');
+            }
+        });
+    });
+
     // Showing / Hiding search option for all users 
     var searchTable = document.getElementById('searchTable');
     var searchAllUsersBtn = document.getElementById('searchAllUsersBtn');
@@ -198,13 +260,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
     removeButtons.forEach(function(button) {
         button.addEventListener('click', function(event) {
+            console.log(button);
             // Get the parent filter div
             const filterDiv = button.closest('.filter');
             const key = filterDiv.getAttribute('data-key');
-            const value = filterDiv.getAttribute('data-value');
-
-            // Log the filter to be removed (for debugging)
-            
+            const value = filterDiv.getAttribute('data-value');          
 
             // Find the matching filter option in the custom-select div
             const filterOptions = document.querySelectorAll('.custom-select .select-items div');
@@ -221,6 +281,20 @@ document.addEventListener("DOMContentLoaded", function () {
                 }else if(key === 'query'){
                     document.getElementById('searchTable').value = '';
                     document.getElementById('searchAllUsersBtn').click();
+                }else if(key === 'sort_by'){
+                    const sortHiddenFilters = document.querySelectorAll('.sort-hidden-filters');
+                    sortHiddenFilters.forEach(function(filter) {
+                        filter.value = '';
+                    });
+                    if(document.getElementById('searchTable').value != ''){
+                        setTimeout(() => {
+                            document.getElementById('sortForm').submit();
+                        }, 100);
+                    }else{
+                        setTimeout(() => {
+                            document.getElementById('filtersForm').submit();
+                        }, 100);
+                    }
                 }
             });
         });
@@ -233,23 +307,36 @@ document.addEventListener("DOMContentLoaded", function () {
             // Get the parent filter div
             const filterDiv = button.closest('.filter');
             const key = filterDiv.getAttribute('data-key');
-            const value = filterDiv.getAttribute('data-value');
-
-            // Log the filter to be removed (for debugging)
-            
+            const value = filterDiv.getAttribute('data-value');      
 
             // Find the matching filter option in the custom-select div
             const filterOptions = document.querySelectorAll('.custom-select .select-items div');
+              
             filterOptions.forEach(function(option) {
                 if (option.getAttribute('data-name') === 'filter_' + key) {
                     option.click();                   
-
                     openPopup('filtersPopup');
                 }else if(key === 'query'){
                     document.getElementById('searchTable').focus();
+                }else if(key === 'sort_by'){
+                    document.getElementById('sortButton').click();
                 }
             });
         });
+    });
+
+    // Get the active sort value from the hidden input
+    if(document.getElementById('activeSort')){
+        var activeSort = document.getElementById('activeSort').value;
+    }
+
+    // Loop through each sort-item-wrapper to find the matching sort option
+    document.querySelectorAll('.sort-item-wrapper').forEach(function(sortItemWrapper) {
+        var radioInput = sortItemWrapper.querySelector('.radio-button__input');
+        var labelDiv = sortItemWrapper.querySelector('.radio-button__label div');
+        if (labelDiv && labelDiv.innerText === activeSort) {
+            radioInput.checked = true;
+        }
     });
 });
 
